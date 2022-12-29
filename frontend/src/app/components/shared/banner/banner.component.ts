4b0d8a90c988b161ef755d61797883535e237c28
Tibo Pfeifer <tibo.pfeifer@gmail.com>
2022-12-29 17:34:06 +0100

    feat_cart_component: Cart details & banner adjustments

--- frontend/src/app/components/shared/banner/banner.component.ts
+++ frontend/src/app/components/shared/banner/banner.component.ts
@@ -10,6 +10,7 @@ const ACOUSTICS_URL = `url(${IMAGE_URL}acoustic-banner.png)`;
 const BASS_URL = `url(${IMAGE_URL}bass-banner.png)`;
 const ACCESSORY_URL = `url(${IMAGE_URL}accessories-banner.png)`;
 const PRODUCT_DETAILS_URL = `url(${IMAGE_URL}product-details-banner.png)`;
+const CART_DETAILS = `url(${IMAGE_URL}cart-banner.png)`;
 
 @Component({
   selector: 'app-banner',
@@ -21,14 +22,16 @@ export class BannerComponent implements OnInit {
   searchMode: boolean = false;
   productView: boolean = false;
   allProductsView: boolean = false;
-  imageUrls: string[] = [
-    PRODUCTS_URL,
-    GUITARS_URL,
-    ACOUSTICS_URL,
-    BASS_URL,
-    ACCESSORY_URL,
-    PRODUCT_DETAILS_URL
-  ]
+  cartDetailsView: boolean = false;
+  imageUrls: {[key: string]: string} = {
+    PRODUCTS: PRODUCTS_URL,
+    GUITARS: GUITARS_URL,
+    ACOUSTICS: ACOUSTICS_URL,
+    BASSES: BASS_URL,
+    ACCESSORIES: ACCESSORY_URL,
+    PRODUCT_DETAILS: PRODUCT_DETAILS_URL,
+    CART_DETAILS: CART_DETAILS
+  }
   currentCategory: number = 0;
 
 
@@ -38,6 +41,7 @@ export class BannerComponent implements OnInit {
     this.searchMode = this.route.snapshot.paramMap.has('keyword');
     this.allProductsView = this.router.url.includes('products') && !this.route.snapshot.paramMap.has('id');
     this.productView = this.router.url.includes('products') && this.route.snapshot.paramMap.has('id');
+    this.cartDetailsView = this.router.url.includes('cart-details');
 
     this.route.params.subscribe(() => {
       if(this.route.snapshot.paramMap.has('id') && !this.router.url.includes('products')){
